f9e9cc22a04f4da1d1f42c77deb52692c07733a1
Kir Tro <trokir@outlook.com>
2024-05-21 19:08:31 -0400

    Add Transaction

---
+++ src/test/java/com/kirtro/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.kirtro.spring.integration.database.repository;
+import com.kirtro.spring.database.entity.Company;
+import com.kirtro.spring.integration.annotation.IT;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@IT
+@RequiredArgsConstructor
+@Transactional
+//@Commit
+class CompanyRepositoryTest {
+
+    private final EntityManager entityManager;
+
+    @Test
+    void findById() {
+        var company = entityManager.find(Company.class, 1);
+        assertNotNull(company);
+        assertThat(company.getLocales()).hasSize(2);
+    }
+
+    @Test
+    void save() {
+        var company = Company.builder()
+                .name("Apple1")
+                .locales(Map.of(
+                        "ru", "Apple описание",
+                        "en", "Apple description"
+                ))
+                .build();
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+}
